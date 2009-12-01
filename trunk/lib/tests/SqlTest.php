0e249c8ddc2e6bab5f45492670cb0dd35a20455f
jaceju <jaceju@4ef5f83e-d8a3-11de-9872-ef835a306e00>
2009-12-01 12:58:03 +0000

    加入測試版 Insert 語法產生器

--- trunk/lib/tests/SqlTest.php
+++ trunk/lib/tests/SqlTest.php
@@ -9,58 +9,62 @@ class GoEz_SqlTest extends PHPUnit_Framework_TestCase
 {
     public function testSelect()
     {
-        $select = GoEz_Sql::select()
+        $sql = GoEz_Sql::select()
+                       ->from('users');
+        $this->assertEquals('SELECT * FROM `users`', (string) $sql);
+
+        $sql = GoEz_Sql::select('name')
                           ->from('users');
-        $this->assertEquals('SELECT * FROM `users`', (string) $select);
+        $this->assertEquals('SELECT `name` FROM `users`', (string) $sql);
 
-        $select = GoEz_Sql::select('name')
-                          ->from('users');
-        $this->assertEquals('SELECT `name` FROM `users`', (string) $select);
+        $sql = GoEz_Sql::select(array('name', 'age'))
+                       ->from('users');
+        $this->assertEquals('SELECT `name`, `age` FROM `users`', (string) $sql);
 
-        $select = GoEz_Sql::select(array('name', 'age'))
-                          ->from('users');
-        $this->assertEquals('SELECT `name`, `age` FROM `users`', (string) $select);
+        $sql = GoEz_Sql::select()
+                       ->distinct()
+                       ->from('users');
+        $this->assertEquals('SELECT DISTINCT * FROM `users`', (string) $sql);
 
-        $select = GoEz_Sql::select()
-                          ->distinct()
-                          ->from('users');
-        $this->assertEquals('SELECT DISTINCT * FROM `users`', (string) $select);
+        $sql = GoEz_Sql::select(array('name', 'age'))
+                       ->distinct()
+                       ->from('users');
+        $this->assertEquals('SELECT DISTINCT `name`, `age` FROM `users`', (string) $sql);
 
-        $select = GoEz_Sql::select(array('name', 'age'))
-                          ->distinct()
-                          ->from('users');
-        $this->assertEquals('SELECT DISTINCT `name`, `age` FROM `users`', (string) $select);
+        $sql = GoEz_Sql::select()
+                       ->from('users')
+                       ->where('name = ?', 'John')
+                       ->where('age = ?', 20);
+        $this->assertEquals("SELECT * FROM `users` WHERE (name = 'John') AND (age = 20)", (string) $sql);
 
-        $select = GoEz_Sql::select()
-                          ->from('users')
-                          ->where('name = ?', 'John')
-                          ->where('age = ?', 20);
-        $this->assertEquals("SELECT * FROM `users` WHERE (name = 'John') AND (age = 20)", (string) $select);
+        $sql = GoEz_Sql::select()
+                       ->from('users')
+                       ->where('name = ?', 'John')
+                       ->where('age = ?', 20)
+                       ->group('name');
+        $this->assertEquals("SELECT * FROM `users` WHERE (name = 'John') AND (age = 20) GROUP BY `name`", (string) $sql);
 
-        $select = GoEz_Sql::select()
-                          ->from('users')
-                          ->where('name = ?', 'John')
-                          ->where('age = ?', 20)
-                          ->group('name');
-        $this->assertEquals("SELECT * FROM `users` WHERE (name = 'John') AND (age = 20) GROUP BY `name`", (string) $select);
+        $sql = GoEz_Sql::select()
+                       ->from('users')
+                       ->where('name = ?', 'John')
+                       ->group('name')
+                       ->order('age');
+        $this->assertEquals("SELECT * FROM `users` WHERE (name = 'John') GROUP BY `name` ORDER BY `age` ASC", (string) $sql);
 
-        $select = GoEz_Sql::select()
-                          ->from('users')
-                          ->where('name = ?', 'John')
-                          ->group('name')
-                          ->order('age');
-        $this->assertEquals("SELECT * FROM `users` WHERE (name = 'John') GROUP BY `name` ORDER BY `age` ASC", (string) $select);
-
-        $select = GoEz_Sql::select()
-                          ->from('users')
-                          ->where('name = ?', 'John')
-                          ->group('name')
-                          ->order('age', 'DESC');
-        $this->assertEquals("SELECT * FROM `users` WHERE (name = 'John') GROUP BY `name` ORDER BY `age` DESC", (string) $select);
+        $sql = GoEz_Sql::select()
+                       ->from('users')
+                       ->where('name = ?', 'John')
+                       ->group('name')
+                       ->order('age', 'DESC');
+        $this->assertEquals("SELECT * FROM `users` WHERE (name = 'John') GROUP BY `name` ORDER BY `age` DESC", (string) $sql);
     }
 
     public function testInsert()
     {
-
+        $sql = GoEz_Sql::insert('users', array(
+            'name' => 'John',
+            'age' => 20,
+        ));
+        $this->assertEquals("INSERT INTO `users` (`name`, `age`) VALUES ('John', 20)", (string) $sql);
     }
 }
