e72a0d89d7359e63641ff4f32edd644f6018a714
jaceju <jaceju@4ef5f83e-d8a3-11de-9872-ef835a306e00>
2009-12-30 03:17:12 +0000

    修正在根目錄下無法正確取得 action name 的問題

--- trunk/lib/GoEz/Router/Rewrite.php
+++ trunk/lib/GoEz/Router/Rewrite.php
@@ -27,12 +27,15 @@ class GoEz_Router_Rewrite extends GoEz_Router
      * </code>
      *
      */
-	protected function _parseUrl()
-	{
+    protected function _parseUrl()
+    {
         $baseDir = basename(APP_ROOT_PATH);
         $currDir = str_replace('index.php', '', $_SERVER['REQUEST_URI']);
-        $pattern = '/^\/' . $baseDir . '\/*(.*)$/';
+        $pattern = '/^\/' . preg_quote($baseDir, '/') . '\/*(.*)$/';
         preg_match($pattern, $currDir, $matches);
+        if (empty($matches)) { // 如果是根目錄
+            $matches = array('', ltrim($currDir, '/'));
+        }
         $tickets = isset($matches[1]) ? explode('/', $matches[1]) : array ('', '');
         $this->_controller = ($tickets[0]) ? strtolower($tickets[0]) : 'index';
         $this->_action = (isset($tickets[1]) && $tickets[1]) ? strtolower($tickets[1]) : 'index';
