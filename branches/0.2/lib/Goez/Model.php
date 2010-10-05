77590a67f61f195de6892256ad0549229b5ea852
jaceju <jaceju@4ef5f83e-d8a3-11de-9872-ef835a306e00>
2010-10-05 10:51:47 +0000

    加入 Model ，並將 Controller 的 Db 移除

---
+++ branches/0.2/lib/Goez/Model.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Goez
+ *
+ * @package    Goez
+ * @copyright  Copyright (c) 2008-2012 Wabow Information Inc. (http://www.wabow.com)
+ * @license    New BSD License
+ * @version    $Id$
+ */
+
+/**
+ * Model
+ *
+ * @package    Goez
+ * @copyright  Copyright (c) 2008-2012 Wabow Information Inc. (http://www.wabow.com)
+ * @license    New BSD License
+ */
+class Goez_Model
+{
+    /**
+     * @var Goez_Db
+     */
+    protected static $_defaultDb = null;
+
+    /**
+     * @param Goez_Db $db
+     */
+    public static function setDefaultAdapter(Goez_Db $db)
+    {
+        self::$_defaultDb = $db;
+    }
+
+    /**
+     * @return Goez_Db
+     */
+    public static function getDefaultAdapter()
+    {
+        return self::$_defaultDb;
+    }
+
+    /**
+     * @var Goez_Db
+     */
+    protected $_db = null;
+
+    /**
+     * @param Goez $db
+     */
+    public function setDb(Goez $db)
+    {
+        $this->_db = $db;
+    }
+
+    /**
+     * @return Goez_Db
+     */
+    public function getDb()
+    {
+        return $this->_db;
+    }
+
+    /**
+     * Constructor
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = array())
+    {
+        $this->_setConfig($config);
+        $this->_setup();
+        $this->init();
+    }
+
+    /**
+     * @var array
+     */
+    protected $_config = array();
+
+    /**
+     * @param array $config
+     */
+    public function setConfig(array $config = array())
+    {
+        $this->_config = $config;
+    }
+
+    /**
+     * 設定資料庫連線
+     *
+     */
+    protected function _setup()
+    {
+        $this->_db = self::getDefaultAdapter();
+    }
+
+    /**
+     * 初始化 (Hook)
+     * 
+     */
+    public function init()
+    {
+
+    }
+}
